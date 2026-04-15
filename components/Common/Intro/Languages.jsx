3bf4de3b93c067f503c4f32a4b23afabf7bf7e88
scottwillats234 <scottwillats234@gmail.com>
2026-04-15 13:59:17 -0700

    Update Languages.jsx

--- components/Common/Intro/Languages.jsx
+++ components/Common/Intro/Languages.jsx
@@ -20,17 +20,7 @@ const Languages = () => {
     return (
         <div className="flex flex-col space-y-1 pt-6">
             <div className="flex flex-col gap-y-4">
-                <span className='text-Snow text-xs font-bold'>Languages</span>
-                <div className="flex flex-row items-center justify-center space-x-6">
-                    <div className="flex flex-col items-center justify-center gap-y-2">
-                        <Progress strokeColor="#1fdf64" type="circle" percent={urdu} size={75} />
-                        <span className='text-xs font-bold text-Snow'>English</span>
-                    </div>
-                    <div className="flex flex-col items-center justify-center gap-y-2">
-                        <Progress strokeColor="#1fdf64" type="circle" percent={english} size={75} />
-                        <span className='text-xs font-bold text-Snow'>Spain</span>
-                    </div>
-                </div>
+               
             </div>
 
         </div>
