51fa575e47eec6212a3f23518609355c1cd24f2e
scottwillats234 <scottwillats234@gmail.com>
2026-04-15 13:57:17 -0700

    Update expertise.js

--- pages/api/expertise.js
+++ pages/api/expertise.js
@@ -1,33 +1,33 @@
 const expertise = [
     {
         id: 0,
-        title: 'E-Commerce Development',
-        desc: 'My mission is to build an innovation and attractive e-commerce site that has great SEO, high performance and good UX.',
+        title: 'FullStack Development',
+        desc: 'I work with modern technologies like React, Node.js, MongoDB, and cloud services to deliver clean, efficient, and maintainable solutions. I focus on performance, usability, and writing high-quality code that is ready for production.',
     },
     {
         id: 1,
-        title: 'NFT Marketplace Development',
-        desc: "Whether it's a simple landing page with a mint function or a beautiful, complex, in-depth website I deliver A+ quality.",
+        title: 'Python Development',
+        desc: "I focus on writing clean, efficient, and maintainable code, with hands-on experience in frameworks like Django and FastAPI, as well as working with cloud services and databases.",
     },
     {
         id: 2,
-        title: 'Dex/Defi Development',
-        desc: 'Get your own platform with me where you can trade and exchange your coins and bitcoins in this amazing crypto universe.',
+        title: 'BlockChain Development',
+        desc: 'I work with Solidity, Web3, and modern blockchain frameworks to create secure, scalable, and efficient solutions.',
     },
     {
         id: 3,
-        title: 'Mobile Application Development',
-        desc: "Using cross platform language like Ionic and react native, android and swift, you can experiment with your ideas on your phone.",
+        title: 'Mobile Design',
+        desc: "I focus on clean, modern design, usability, and seamless user experiences, turning ideas into high-quality, production-ready designs using tools like Figma and industry best practices.",
     },
     {
         id: 4,
-        title: 'Smart Contract Development',
-        desc: "When ready we will deploy your contract to Rinkeby for testing and then to Mainnet for launch. Your images and metadata will never be exposed and I will be on-call in case any issues arise.",
+        title: 'E-Commerce Development',
+        desc: "I work with platforms like Shopify and custom stacks using React and Node.js to deliver fast, secure, and user-friendly shopping experiences.",
     },
     {
         id: 5,
-        title: 'Game Development',
-        desc: "Make a P2E Game with your NFT and FT and play around with it, and make some money. so funny!!!"
+        title: 'AI Development',
+        desc: "MI work with Python, LLM APIs, and data pipelines to create scalable, real-world solutions. I focus on clean architecture, reliable performance"
     }
 ]
 
